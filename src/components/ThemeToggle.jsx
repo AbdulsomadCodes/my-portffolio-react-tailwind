e9bfa8bc8e2b478fad19acfc0159afd58b8d2ff8
Abdulsomad <abdulganiyola6@gmail.com>
2025-11-13 23:11:26 +0100

    Misplet hidden in themetoggle

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -32,7 +32,7 @@ export const ThemeToggle = () => {
 
     return (
         <button onClick={toggleTheme} className={cn(
-            "fixed max-sm:hiddn top-5 right-5 p-2 z-50 rounded-full transition-colors duration-300",
+            "fixed max-sm:hidden top-5 right-5 p-2 z-50 rounded-full transition-colors duration-300",
              "focus:outline-hidden"
             )}>
 
